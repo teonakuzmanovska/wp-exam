da122ad9224e6ea1eabe73779962b5e56518ce65
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2023-06-21 03:28:58 +0200

    initial

---
+++ src/main/java/mk/ukim/finki/wp/exam/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package mk.ukim.finki.wp.exam.example.service.impl;
+
+import mk.ukim.finki.wp.exam.example.model.Role;
+import mk.ukim.finki.wp.exam.example.model.User;
+import mk.ukim.finki.wp.exam.example.repository.UserRepository;
+import mk.ukim.finki.wp.exam.example.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User create(String username, String password, Role role) {
+        String encryptedPassword = this.passwordEncoder.encode(password);
+        User user = new User(username, encryptedPassword, role);
+
+        return this.userRepository.save(user);
+    }
+}
